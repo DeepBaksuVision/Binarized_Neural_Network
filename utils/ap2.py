70e1f08c272da38e59c4935603dfa6348980a28f
chromatices <56014940+chromatices@users.noreply.github.com>
2019-11-22 23:00:20 +0900

    Feature/#15 (#16)
    * AP2 구현 + tensor bit shift 구현
    
    * file names changed and codes edit
    
    * for문 제거 및 ap2 torch.no_grad() 테스트
    
    * torch.no_grad() 에 대한 테스트 코드 작성
    
    * bit_shift_tensor 중 np.where~ 함수 분기

--- utils/ap2.py
+++ utils/ap2.py
@@ -1,10 +1,24 @@
 import torch
 
 
-def approximate_power_of_two(x: torch.tensor):
+def approximate_power_of_two(x: torch.tensor) -> torch.tensor:
     with torch.no_grad():
         return torch.sign(x) * 2 ** torch.round(torch.log2(torch.abs(x)))
 
 
 if __name__ == "__main__":
-    print(approximate_power_of_two(torch.randn(3,3)))
+    a = torch.randn(3, 3, requires_grad=True)
+    b = a * a
+
+    print("tensor a : \n", a)
+    print("\na.requres_grad : ",a.requires_grad)
+    print("\na.grad : ",a.grad)
+    print("\na.grad_fn : ",a.grad_fn)
+
+    print("\n a * a : \n", b)
+    print("\n(a*a).grad : ", b.grad)
+    print("\n(a*a).grad_fn : ",b.grad_fn)
+
+    print("approximate_power_of_two(a).requires_grad : ",approximate_power_of_two(a).requires_grad)
+    print("approximate_power_of_two(a).grad : ",approximate_power_of_two(a).grad)
+    print("approximate_power_of_two(a).grad_fn : ",approximate_power_of_two(a).grad_fn)
